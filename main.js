65555cdfa74521c80031cf851ed93c1ffada1c7e
Kirill Avramenko <mirecsy@gmail.com>
2019-08-04 21:51:08 -0700

    Add navigation toggle

---
+++ main.js
@@ -0,0 +1,19 @@
+function clickEvent(event) {
+  var targetElement = event.target || event.srcElement;
+
+  if (targetElement == currSelectedLink) return;
+
+  currSelectedLink.className = "";
+  targetElement.className = "active";
+  currSelectedLink = targetElement;
+  console.log(targetElement);
+}
+
+let navLinks = document.querySelectorAll(".nav a");
+let currSelectedLink;
+navLinks.forEach(function(elem) {
+  if (elem.className == "active") {
+    currSelectedLink = elem;
+  }
+  elem.addEventListener("click", clickEvent);
+});
