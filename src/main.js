bf215470dc3b3e681b0202048933453eb773030f
Kirill Avramenko <mirecsy@gmail.com>
2019-08-10 17:28:58 -0700

    create programetically dom objects

--- src/main.js
+++ src/main.js
@@ -1,21 +1,3 @@
-"use strict";
+import attachWorkExpToCardContent from "/elementsCreator.js";
 
-function clickEvent(event) {
-  var targetElement = event.target || event.srcElement;
-
-  if (targetElement == currSelectedLink) return;
-
-  currSelectedLink.className = "";
-  targetElement.className = "active";
-  currSelectedLink = targetElement;
-  console.log(targetElement);
-}
-
-let navLinks = document.querySelectorAll(".nav a");
-let currSelectedLink;
-navLinks.forEach(function(elem) {
-  if (elem.className == "active") {
-    currSelectedLink = elem;
-  }
-  elem.addEventListener("click", clickEvent);
-});
+attachWorkExpToCardContent("");
