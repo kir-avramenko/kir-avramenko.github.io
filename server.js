4a98e9f12f2c4a7d171c78900f8366d9b4780f0a
Kirill Avramenko <mirecsy@gmail.com>
2019-09-14 13:27:00 -0700

    remove pics, format server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-"use strict";
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
-var app = express();
-app.use(express.static(path.join(__dirname, "/dist")));
+const app = express();
+app.use(express.static(path.join(__dirname, '/dist')));
 
-app.get("/*", function(req, res) {
-  res.sendFile("index.html", { root: path.join(__dirname, "/dist") });
+app.get('/*', function getAll(req, res) {
+  res.sendFile('index.html', { root: path.join(__dirname, '/dist') });
 });
 
 const port = process.env.PORT || 3000;
